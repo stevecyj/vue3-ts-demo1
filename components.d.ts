62d1e1c1943b585e656351c1f417315b160f877b
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-09-26 11:04:29 +0800

    feat: add HelloButton component to App.vue
    - Add a new component called HelloButton.vue
    - Import and register the HelloButton component in App.vue
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,7 @@ declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElRow: typeof import('element-plus/es')['ElRow']
+    HelloButton: typeof import('./src/components/HelloButton.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
-  <el-row class="mb-4">
-    <el-button>Default</el-button>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
-    <el-button type="danger">Danger</el-button>
-  </el-row>
+  <HelloButton />
 </template>
 
 <style scoped>
