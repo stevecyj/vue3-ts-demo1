cab8b7fe27a3b2ea78ff2e4ab9b068f7a4a671d2
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-09-26 17:45:34 +0800

    refactor: refactor menu functionality
    - Remove unused imports and functions
    - Replace reactive menuItems with menuItems from useMenu
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/components/layout/navMenu.vue
+++ src/components/layout/navMenu.vue
@@ -1,24 +1,8 @@
 <script setup lang='ts'>
-import { rec, reactive } from 'vue'
-import {
-  Document,
-  Menu as IconMenu,
-  Location,
-  Setting,
-} from '@element-plus/icons-vue'
-const handleOpen = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
-const handleClose = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
+// import { rec, reactive } from 'vue'
+import { useMenu } from './useMenu'
+const { menuItems } = useMenu()
 
-const menuItems = reactive([
-  { name: '首頁', url: '/index', icon: Document },
-  { name: '監控', url: '/monitor', icon: Location },
-  { name: '訂單', url: '/order', icon: Setting },
-  { name: '統計', url: '/census', icon: IconMenu },
-])
 </script>
 
 <template>
