cbd9d27c934bd02ad663d0eeca92d9c8671121fe
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-09-27 12:02:46 +0800

    single attribute per line

--- src/components/layout/navMenu.vue
+++ src/components/layout/navMenu.vue
@@ -1,19 +1,22 @@
-<script setup lang='ts'>
+<script setup lang="ts">
 // import { rec, reactive } from 'vue'
-import { useMenu } from './useMenu'
-const { menuItems } = useMenu()
-
+import { useMenu } from "./useMenu";
+const { menuItems } = useMenu();
 </script>
 
 <template>
   <el-aside width="200px">
-    <el-menu default-active="/index"
-             router>
-      <el-menu-item :index='v.url'
-                    v-for='v in menuItems'
-                    :key='v.url'>
+    <el-menu
+      default-active="/index"
+      router
+    >
+      <el-menu-item
+        v-for="v in menuItems"
+        :index="v.url"
+        :key="v.url"
+      >
         <el-icon>
-          <component :is='v.icon' />
+          <component :is="v.icon" />
         </el-icon>
         <span>{{ v.name }}</span>
       </el-menu-item>
