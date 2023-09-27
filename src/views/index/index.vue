a8198c0ad1b90544b5eca7ea002bc8a40fd37bc9
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-09-27 15:35:09 +0800

    feat: update tableData structure and labels
    - Added an interface definition for `ITableData`
    - Updated the data type of `tableData` to be an array of `ITableData`
    - Updated the values of `date`, `name`, and `address` in the `tableData` array
    - Updated the label of the `el-table-column` for `date` to be "日期"
    - Updated the label of the `el-table-column` for `name` to be "姓名"
    - Updated the label of the `el-table-column` for `address` to be "地址"
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,32 +1,53 @@
 <script lang="ts" setup>
-const tableData = [
+// 類型定義
+interface ITableData {
+  date: string;
+  name: string;
+  address: string;
+}
+// 表格数据類型添加
+const tableData: Array<ITableData> = [
   {
-    date: '2016-05-03',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+    date: "2016-05-03",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
   },
   {
-    date: '2016-05-02',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+    date: "2016-05-02",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
   },
   {
-    date: '2016-05-04',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+    date: "2016-05-04",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
   },
   {
-    date: '2016-05-01',
-    name: 'Tom',
-    address: 'No. 189, Grove St, Los Angeles',
+    date: "2016-05-01",
+    name: "Tom",
+    address: "No. 189, Grove St, Los Angeles",
   },
-]
+];
 </script>
 
 <template>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="date" label="Date" width="180" />
-    <el-table-column prop="name" label="Name" width="180" />
-    <el-table-column prop="address" label="Address" />
+  <el-table
+    :data="tableData"
+    style="width: 100%"
+  >
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180"
+    />
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180"
+    />
+    <el-table-column
+      prop="address"
+      label="地址"
+    />
   </el-table>
 </template>
