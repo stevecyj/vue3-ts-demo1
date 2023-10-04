8b8779ce0f8fda480eeacdc949fac66ea9b35c66
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-10-04 10:06:34 +0800

    docs: add function type aliases and void to TSREADME.md, and update funType.ts
    - Add `函數類型別名` section to TSREADME.md
    - Add `void` section to TSREADME.md
    - Remove `FnType` type alias from funType.ts
    - Add `sayHi` function to funType.ts
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/ts/TSREADME.md
+++ src/ts/TSREADME.md
@@ -2,6 +2,8 @@
 
 ## function
 
+### 函數類型別名
+
 ```javascript
 // 函數類型別名
 // 通常給箭頭函數、函數表達式使用
@@ -15,4 +17,15 @@ const sub: FnType = (x, y) => x - y;
 
 sub(1, 2);
 sub("123", "456");
+```
+
+### void
+
+```JS
+// 定義一個「列印文字」的函數，無需回傳值
+// ts 會自動推論 sayHi 的型別為 (name: string) => void
+// ts 中寫 : undefined 必需回傳 undefined，寫 : void 可以不回傳
+const sayHi = (name: string): void => {
+    console.log(`Hi, ${name}`);
+};
 ```
--- src/ts/funType.ts
+++ src/ts/funType.ts
@@ -1,14 +1,8 @@
 {
-  // 函數類型別名
-  // 通常給箭頭函數、函數表達式使用
-  type FnType = (x: number, y: number) => number;
-
-  const fn: FnType = function add(x, y) {
-    return x + y;
+  // 定義一個「列印文字」的函數，無需回傳值
+  // ts 會自動推論 sayHi 的型別為 (name: string) => void
+  // ts 中寫 : undefined 必需回傳 undefined，寫 : void 可以不回傳
+  const sayHi = (name: string): void => {
+    console.log(`Hi, ${name}`);
   };
-
-  const sub: FnType = (x, y) => x - y;
-
-  sub(1, 2);
-  sub("123", "456");
 }
