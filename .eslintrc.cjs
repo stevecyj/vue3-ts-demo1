4d7d0f17049d8fa24b0d75e85bdbebed7332b9d1
stevecyj <ycsteven.tsao@gmail.com>
2023-10-06 22:22:56 +0800

    chore: update ESLint and Prettier configurations
    - Added a new file `.eslintrc.cjs`
    - Added a new file `.prettierrc`
    - Modified the file `package.json` by updating the following dependencies:
      - `@typescript-eslint/eslint-plugin` to version `6.7.4`
      - `@typescript-eslint/parser` to version `6.7.4`
      - `eslint` to version `8.50.0`
      - `eslint-config-standard-with-typescript` to version `39.1.0`
      - `eslint-plugin-import` to version `2.28.1`
      - `eslint-plugin-n` to version `16.1.0`
      - `eslint-plugin-promise` to version `6.1.1`
      - `eslint-plugin-vue` to version `9.17.0`
      - `prettier` to version `3.0.3`
      - `typescript` to version `5.2.2`
    - Modified the file `tsconfig.json` by adding `.eslintrc.cjs` to the `include` section
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['standard-with-typescript', 'plugin:vue/vue3-essential'],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['vue'],
+  rules: {}
+}
