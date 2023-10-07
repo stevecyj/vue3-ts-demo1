5480730d92a003402d5fec1b0b465dc46a0fd9a5
stevecyj <ycsteven.tsao@gmail.com>
2023-10-08 05:45:58 +0800

    chore: refactor Vite configuration file
    - Added `vite.config.ts` to the list of files in `tsconfig.json`
    - Removed unnecessary import statements in `vite.config.ts`
    - Simplified the configuration object in `vite.config.ts`
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
-import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 // import { viteMockServe } from "vite-plugin-mock";
 
 // https://vitejs.dev/config/
@@ -10,15 +10,15 @@ export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver()]
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
-    }),
+      resolvers: [ElementPlusResolver()]
+    })
     // viteMockServe({
     //   // default
     //   mockPath: "src/mock",
     //   enable: true,
     // }),
-  ],
+  ]
 });
