4ebbed65dbb66d9c3abeb3f3c0e527101825b927
stevecyj <ycsteven.tsao@gmail.com>
2023-10-08 05:35:31 +0800

    feat: implement data fetching, filtering, and dialog form functionality
    - Add a new file `src/views/index/HomeIndex.vue`
    - Initialize variables and imports in the `<script>` section
    - Fetch `ITableData` and `INewsList` data from the server
    - Set up computed properties for date filtering
    - Implement a filter method for the table
    - Add a method to open a dialog form for adding or editing data
    - Render a button to open the dialog form
    - Render an icon
    - Render an `<el-table>` component with columns and data
    - Render a dialog form with form fields and buttons
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

---
+++ src/views/index/HomeIndex.vue
@@ -0,0 +1,152 @@
+<script setup lang="ts">
+// 類型定義
+import { ref, onMounted, computed } from 'vue';
+import axios from '../../http/http';
+import { type ITableData, type INewsList } from '../../models/index';
+
+const tableData = ref<ITableData[]>([]); // 表格數據
+const dialogFormVisible = ref<boolean>(false); // 彈框顯示
+const form = ref({});
+const formLabelWidth = '140px';
+
+const getNewsDemo = async (): Promise<void> => {
+  const { data } = await axios.request<{ data: INewsList[] }>(
+    'get',
+    '/news-demo'
+  );
+  console.log('getNewsDemo', data);
+};
+
+const getTableData = async (): Promise<void> => {
+  const { data } = await axios.request<{ data: ITableData[] }>(
+    'get',
+    '/data/query'
+  );
+  tableData.value = data;
+};
+
+onMounted(async () => {
+  await getTableData();
+  await getNewsDemo();
+});
+
+// 處理日期動態數據
+const dateList = computed(() => {
+  // 去重
+  return Array.from(new Set(tableData.value.map((item) => item.date))).map(
+    (item) => ({
+      text: item,
+      value: item
+    })
+  );
+});
+// 篩選方法
+const filterHandler = (value: string, row: ITableData) => {
+  return row.date === value;
+};
+
+// 新增、編輯
+const setData = (type: string, row: ITableData | null) => {
+  dialogFormVisible.value = true;
+  type === 'add' ? (form.value = {}) : (form.value = { ...row });
+};
+</script>
+
+<template>
+  <div>
+    <el-button
+      type="success"
+      @click="setData('add', null)"
+      >新增</el-button
+    >
+  </div>
+  <el-icon :size="20">
+    <el-icon-edit />
+  </el-icon>
+  <el-table
+    :data="tableData"
+    style="width: 100%"
+  >
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180"
+      column-key="date"
+      :filters="dateList"
+      :filter-method="filterHandler"
+    />
+    <el-table-column
+      prop="title"
+      label="標題"
+      width="180"
+    />
+    <el-table-column
+      prop="user"
+      label="用戶"
+    />
+    <el-table-column
+      prop="id"
+      label="識別碼"
+    />
+    <el-table-column
+      prop="check"
+      label="狀態"
+    >
+      <template #default="{ row }">
+        <el-tag
+          class="ml-2"
+          :type="row.check ? 'success' : 'warning'"
+        >
+          {{ row.check ? '已完成' : '未完成' }}
+        </el-tag>
+      </template>
+    </el-table-column>
+  </el-table>
+
+  <!-- 彈框 -->
+  <el-dialog
+    v-model="dialogFormVisible"
+    title="新增"
+  >
+    <el-form :model="form">
+      <el-form-item
+        label="Promotion name"
+        :label-width="formLabelWidth"
+      >
+        <el-input
+          v-model="form.name"
+          autocomplete="off"
+        />
+      </el-form-item>
+      <el-form-item
+        label="Zones"
+        :label-width="formLabelWidth"
+      >
+        <el-select
+          v-model="form.region"
+          placeholder="Please select a zone"
+        >
+          <el-option
+            label="Zone No.1"
+            value="shanghai"
+          />
+          <el-option
+            label="Zone No.2"
+            value="beijing"
+          />
+        </el-select>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">Cancel</el-button>
+        <el-button
+          type="primary"
+          @click="dialogFormVisible = false"
+        >
+          Confirm
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
+</template>
